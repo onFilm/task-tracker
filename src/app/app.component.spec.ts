8aa28f0e69ef9a975734eff1ff07c88bfaaa5488
prajwal <mail2prajwal12@gmail.com>
2021-07-04 17:50:47 +0530

    Initial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('task-tracker');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('task-tracker app is running!');
-  });
 });
