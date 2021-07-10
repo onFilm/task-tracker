ff92292c513fe4bb8fbfec495080bd31aba240cc
prajwal <mail2prajwal12@gmail.com>
2021-07-10 14:30:06 +0530

    Adding reminder toggle and css change

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faBell, faBellSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -10,10 +10,19 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
-  faTimes = faTimes;
+  icon: any;
+  iconColor: any = "";
+  
   constructor() { }
 
   ngOnInit(): void {
+    if(this.task.reminder === true) {
+      this.icon = faBell;
+      this.iconColor = "green";
+    } else {
+      this.icon = faBellSlash;
+      this.iconColor = "red";
+    }
   }
 
   onDelete (id: any) {
